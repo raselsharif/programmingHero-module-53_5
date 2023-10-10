bb1bdba1edadef7ec56898be188c186d934dd957
Rasel Sharif <raselsharif9@gmail.com>
2023-10-10 14:54:05 +0600

    added phone api created

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const phones = require("./phones.json")
 const port = 5000;
 
 app.get('/', (req, res)=>{
     res.send("node server")
 })
+app.get("/phones", (req, res)=>{
+    res.send(phones)
+})
+
+app.get('/phones/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    console.log(id);
+    const phone = phones.find(phone => phone.id === id) || {}
+    res.send(phone)
+})
 app.listen(port,()=>{
-    console.log(`server running ${port}`);
+    console.log(`server running on port No: ${port}`);
 })
